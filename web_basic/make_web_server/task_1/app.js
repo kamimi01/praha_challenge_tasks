637631c55e2de9b63e7dc95040f50a84c8565168
kamimi01 <awakaru0122@gmail.com>
2021-01-09 02:58:59 +0900

    docs: コメントをJSDoc風に修正
    #16

--- web_basic/make_web_server/task_1/app.js
+++ web_basic/make_web_server/task_1/app.js
@@ -6,22 +6,30 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// GETリクエスト受けた時、
-// {text: hello world}とjsonをHTTPステータス200で返してください
+/**
+ * Hello-World（POSTMANのリクエスト名）
+ *
+ * @description
+ * GETリクエスト受けた時、{text: hello world}とjsonをHTTPステータス200で返す
+ */
 app.get("/", (req, res) => {
   const resBody = data.HelloWorld
   res.status(200).json(resBody)
 })
 
-// POSTリクエストを受けた時、
-// リクエストbodyに含まれるjsonデータを、レスポンスのbodyに含めて、HTTPステータス201で返してください
+/**
+ * Name-Hoge-201
+ * Name-Hoge-400
+ *
+ * @description
+ * POSTリクエストを受けた時、リクエストbodyに含まれるjsonデータを、レスポンスのbodyに含めて、HTTPステータス201で返す
+ * Content-Typeがapplication/json以外の時は、HTTPステータス400を返す
+ */
 app.post("/", (req, res) => {
   const reqBody = req.body
   const actualContentType = req.headers["content-type"]
   const expectedContentType = "application/json"
 
-  // Content-Typeがapplication/json以外の時は、
-  // HTTPステータス400を返してください
   if (actualContentType != expectedContentType) {
     const resBody = data.BadRequest
     res.status(400).json(resBody)
