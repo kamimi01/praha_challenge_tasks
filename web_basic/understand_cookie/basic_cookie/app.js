8851de564fdf68eac2ca3fe1e82fe1e3a67fc7c9
kamimi01 <awakaru0122@gmail.com>
2021-01-17 13:21:34 +0900

    feat: クッキー設定の実験中
    #17

--- web_basic/understand_cookie/basic_cookie/app.js
+++ web_basic/understand_cookie/basic_cookie/app.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
+  // CORSを許可する（ngrokを使用して公開ドメインを使う場合は、以下の設定だとCORSのエラー出るので注意）
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+
   // 第一引数がname、第二引数がvalue、第三引数はオプション
   res.cookie("name1", "value1")
 
@@ -9,7 +14,7 @@ app.get("/", (req, res) => {
     // ミリ秒で1分に期限を設定
     // max-ageに設定したmsを経過するとクッキーが削除される
     maxAge: 60000,
-    httpOnly: false
+    httpOnly: false,
   })
 
   // 現在時刻を取得
@@ -19,26 +24,31 @@ app.get("/", (req, res) => {
 
   res.cookie("name3", "value3", {
     // expiresに設定した期限を経過するとクッキーが削除される
-    expires: currentDate
+    expires: currentDate,
   })
 
   res.cookie("name4", "value4", {
     // これをtrueにすると、javascriptのdocument.cookieでクッキーが取得できなくなる
-    httpOnly: true
+    httpOnly: true,
   })
 
   res.cookie("name5", "value5", {
     // ここに設定されているドメインのサブドメインにはクッキーが送られる
     // ただ、ここには何も設定しないのが一番安全
-    domain: ".happycookie.com",
+    domain: ".ngrok.io",
   })
 
   res.cookie("name6", "value6", {
     // httpsにのみクッキーが送られる
-    secure: true
+    secure: true,
   })
 
-  res.json()
+  res.cookie("name7", "value7", {
+    // httpsにのみクッキーが送られる
+    sameSite: "lax"
+  })
+
+  res.json({ test: "test" })
 })
 
 app.listen(8000, () => {
