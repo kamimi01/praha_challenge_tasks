3087b55f4f7ca3af38ea3dfe75a9c1d9ef17a12d
kamimi01 <awakaru0122@gmail.com>
2021-03-14 17:09:02 +0900

    feat: Squareコンポーネントのテストを追加
    #46

--- testing_basic/tic-tac-toe/src/components/Square/square.stories.js
+++ testing_basic/tic-tac-toe/src/components/Square/square.stories.js
@@ -4,8 +4,22 @@ import { Square } from "./Square";
 export default {
   title: "Tic-Tac-Toe/Square",
   component: Square,
+  argTypes: { onClick: { action: "Clicked" } },
 };
 
 const Template = (args) => <Square {...args} />;
 
-export const Usual = Template.bind({});
+export const Circle = Template.bind({});
+Circle.args = {
+  value: "半"
+}
+
+export const Cross = Template.bind({});
+Cross.args = {
+  value: "丁"
+}
+
+export const Triangle = Template.bind({})
+Triangle.args = {
+  value: "△"
+}
